c8dbd6c0925279c9d04328101fae911cfb8bcbb6
Johan Lindfors <johan.lindfors@coderox.se>
2019-03-12 14:53:41 +0100

    UWP now working as well

--- dependencies/engine/source/uwp/Application.cpp
+++ dependencies/engine/source/uwp/Application.cpp
@@ -1,118 +1,36 @@
-// WindowsProject2.cpp : Defines the entry point for the application.
-//
-#include <Windows.h>
+#include <winrt/Windows.UI.Xaml.Controls.h>
+#include <winrt/Windows.UI.Xaml.Media.h>
+#include <winrt/Windows.Foundation.h>
 
-#define MAX_LOADSTRING				100
-#define IDS_APP_TITLE				103
-#define IDM_EXIT					105
-#define IDI_WINDOWSPROJECT2			107
-#define IDI_SMALL					108
-#define IDC_WINDOWSPROJECT2			109
+using namespace winrt;
+using namespace Windows::Foundation;
+using namespace Windows::ApplicationModel::Activation;
+using namespace Windows::UI;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Media;
 
-// Global Variables:
-HINSTANCE hInst;                                // current instance
+struct App : ApplicationT<App>
+{
+	void OnLaunched(LaunchActivatedEventArgs const &)
+	{
+		TextBlock block;
 
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+		block.FontFamily(FontFamily(L"Segoe UI Semibold"));
+		block.FontSize(72.0);
+		block.Foreground(SolidColorBrush(Colors::Orange()));
+		block.VerticalAlignment(VerticalAlignment::Center);
+		block.TextAlignment(TextAlignment::Center);
+		block.TextWrapping(TextWrapping::Wrap);
+		block.Text(L"Game Engine Sandbox");
+
+		Window window = Window::Current();
+		window.Content(block);
+		window.Activate();
+	}
+};
 
 void StartUwpApplication()
 {
-	HINSTANCE hInstance = GetModuleHandle(NULL);
-	MyRegisterClass(hInstance);
-
-	// Perform application initialization:
-	if (!InitInstance(hInstance))
-	{
-		return;
-	}
-
-	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_WINDOWSPROJECT2));
-
-	MSG msg;
-
-	// Main message loop:
-	while (GetMessage(&msg, nullptr, 0, 0))
-	{
-		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-		{
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
-}
-
-ATOM MyRegisterClass(HINSTANCE hInstance)
-{
-	WNDCLASSEXW wcex;
-
-	wcex.cbSize = sizeof(WNDCLASSEX);
-
-	wcex.style = CS_HREDRAW | CS_VREDRAW;
-	wcex.lpfnWndProc = WndProc;
-	wcex.cbClsExtra = 0;
-	wcex.cbWndExtra = 0;
-	wcex.hInstance = hInstance;
-	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_WINDOWSPROJECT2));
-	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
-	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_WINDOWSPROJECT2);
-	wcex.lpszClassName = L"GAMEENGINESANDBOX";
-	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-	return RegisterClassExW(&wcex);
-}
-
-BOOL InitInstance(HINSTANCE hInstance)
-{
-	hInst = hInstance; // Store instance handle in our global variable
-
-	HWND hWnd = CreateWindowW(L"GAMEENGINESANDBOX", L"", WS_OVERLAPPEDWINDOW,
-		CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
-
-	if (!hWnd)
-	{
-		return FALSE;
-	}
-
-	ShowWindow(hWnd, 5);
-	UpdateWindow(hWnd);
-
-	return TRUE;
-}
-
-LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message)
-	{
-	case WM_COMMAND:
-	{
-		int wmId = LOWORD(wParam);
-		// Parse the menu selections:
-		switch (wmId)
-		{
-		case IDM_EXIT:
-			DestroyWindow(hWnd);
-			break;
-		default:
-			return DefWindowProc(hWnd, message, wParam, lParam);
-		}
-	}
-	break;
-	case WM_PAINT:
-	{
-		PAINTSTRUCT ps;
-		HDC hdc = BeginPaint(hWnd, &ps);
-		// TODO: Add any drawing code that uses hdc here...
-		EndPaint(hWnd, &ps);
-	}
-	break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-	return 0;
+	Application::Start([](auto &&) { make<App>(); });
 }
