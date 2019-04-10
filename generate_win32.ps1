789f41f4f42af3971bc82fe286291f88c021b20c
Johan Lindfors <johan.lindfors@coderox.se>
2019-04-10 13:52:23 +0200

    Parameterizing the win32 generation with toolchainFile

--- generate_win32.ps1
+++ generate_win32.ps1
@@ -1,3 +1,11 @@
-cmake -E remove_directory build/win32
-cmake -E make_directory build/win32
-cmake -E chdir build/win32 cmake -G "Visual Studio 15 2017" -DCMAKE_TOOLCHAIN_FILE="c:\repos\microsoft\vcpkg\scripts\buildsystems\vcpkg.cmake" -A x64 ../..
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory = $True)]
+    [string]$toolchainFile
+)
+
+$Cmake = "cmake.exe"
+
+& $Cmake @("-E", "remove_directory", "build/win32")
+& $Cmake @("-E", "make_directory", "build/win32")
+& $Cmake @("-E", "chdir", "build/win32", "cmake", "-G", "Visual Studio 15 2017", "-DCMAKE_TOOLCHAIN_FILE='$toolchainFile'", "-A", "x64", "../..")
