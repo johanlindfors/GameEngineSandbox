b76cd3c99daaacbcabece8960db8791b0db4c0a6
Johan Lindfors <johan.lindfors@coderox.se>
2019-04-16 08:10:00 +0200

    Set thread names

--- dependencies/engine/source/uwp/GLPage.cpp
+++ dependencies/engine/source/uwp/GLPage.cpp
@@ -17,9 +17,39 @@ using namespace Windows::System::Threading;
 
 #define SCALE_FACTOR 1.0f
 
+//
+// Usage: SetThreadName ((DWORD)-1, "Game Thread");
+//
+const DWORD MS_VC_EXCEPTION = 0x406D1388;
+#pragma pack(push, 8)
+typedef struct tagTHREADNAME_INFO {
+	DWORD dwType;	 // Must be 0x1000.
+	LPCSTR szName;	// Pointer to name (in user addr space).
+	DWORD dwThreadID; // Thread ID (-1=caller thread).
+	DWORD dwFlags;	// Reserved for future use, must be zero.
+} THREADNAME_INFO;
+#pragma pack(pop)
+void SetThreadName(DWORD dwThreadID, const char* threadName) {
+	THREADNAME_INFO info;
+	info.dwType = 0x1000;
+	info.szName = threadName;
+	info.dwThreadID = dwThreadID;
+	info.dwFlags = 0;
+#pragma warning(push)
+#pragma warning(disable : 6320 6322)
+	__try {
+		RaiseException(MS_VC_EXCEPTION, 0, sizeof(info) / sizeof(ULONG_PTR), (ULONG_PTR*)&info);
+	}
+	__except (EXCEPTION_EXECUTE_HANDLER) {
+	}
+#pragma warning(pop)
+}
+
 GLPage::GLPage(shared_ptr<OpenGLES> openGLES) 
 	: mOpenGLES(openGLES)
 {
+	SetThreadName((DWORD)-1, "UI Thread");
+	
 	mContentRoot.BorderThickness(Thickness{ 0 });
 	mContentRoot.Background(SolidColorBrush(Colors::Black()));
 	mContentRoot.Children().Append(mSwapChainPanel);
@@ -41,6 +71,8 @@ void GLPage::CreateRenderSurface()
 
 void GLPage::RenderLoop(IAsyncAction const& /*action*/)
 {
+	SetThreadName((DWORD)-1, "Game Thread");
+	
 	mOpenGLES->Initialize();
 	CreateRenderSurface();
 	mOpenGLES->MakeCurrent(mRenderSurface);
