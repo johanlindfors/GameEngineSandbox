67f7e25d56bf4fcd1319facd1a321dc23e9e1a91
Johan Lindfors <johan.lindfors@coderox.se>
2019-04-27 09:10:47 +0200

    Resources refactored, now works with Win32

---
+++ dependencies/pipeline/utilities.py
@@ -0,0 +1,2 @@
+VCXPRJ_RESOURCES_SNIPPET = '<ItemGroup Label="resource_pipeline_output"><_CustomResource Include="..\..\..\resources\common\**\*"><Link>resources\%(RecursiveDir)%(FileName)%(Extension)</Link><DeploymentContent>true</DeploymentContent></_CustomResource></ItemGroup><Target BeforeTargets="AssignTargetPaths" Name="_CollectCustomResources"><Message Text="Adding resource: %(_CustomResource.Identity) -&gt; %(_CustomResource.Link)" /><ItemGroup><None Include="@(_CustomResource)" /></ItemGroup></Target>'
+
