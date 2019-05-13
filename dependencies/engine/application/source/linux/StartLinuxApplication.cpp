0096dd0aeeb83d7d7ec32b9ea86753ae7a4d7e73
Johan Lindfors <johan.lindfors@coderox.se>
2019-05-13 06:35:38 +0200

    Compiles but seems to crash on glCompileProgram

--- dependencies/engine/application/source/linux/StartLinuxApplication.cpp
+++ dependencies/engine/application/source/linux/StartLinuxApplication.cpp
@@ -1,61 +1,45 @@
-
-/* Copyright (c) Mark J. Kilgard, 1996. */
-
-/* This program is freely distributable without licensing fees 
-   and is provided without guarantee or warrantee expressed or 
-   implied. This program is -not- in the public domain. */
-
-/* This program is a response to a question posed by Gil Colgate
-   <gcolgate@sirius.com> about how lengthy a program is required using
-   OpenGL compared to using  Direct3D immediate mode to "draw a
-   triangle at screen coordinates 0,0, to 200,200 to 20,200, and I
-   want it to be blue at the top vertex, red at the left vertex, and
-   green at the right vertex".  I'm not sure how long the Direct3D
-   program is; Gil has used Direct3D and his guess is "about 3000
-   lines of code". */
-
-/* X compile line: cc -o simple simple.c -lglut -lGLU -lGL -lXmu -lXext -lX11 -lm */
-
+#include "game-loop/GameLoop.h"
 #include "glwrapper.h"
 #include <GL/glut.h>
+#include <memory>
+#include "input/IInputManager.h"
+#include "application/Config.h"
+#include "IOC.hpp"
+
+using namespace Engine;
+using namespace Utilities;
+
+namespace {
+  std::unique_ptr<GameLoop> g_gameLoop;
+};
 
 void
 reshape(int w, int h)
 {
-  /* Because Gil specified "screen coordinates" (presumably with an
-     upper-left origin), this short bit of code sets up the coordinate
-     system to correspond to actual window coodrinates.  This code
-     wouldn't be required if you chose a (more typical in 3D) abstract
-     coordinate system. */
-
-  glViewport(0, 0, w, h);       /* Establish viewing area to cover entire window. */
-  glMatrixMode(GL_PROJECTION);  /* Start modifying the projection matrix. */
-  glLoadIdentity();             /* Reset project matrix. */
-  glOrtho(0, w, 0, h, -1, 1);   /* Map abstract coords directly to window coords. */
-  glScalef(1, -1, 1);           /* Invert Y axis so increasing Y goes down. */
-  glTranslatef(0, -h, 0);       /* Shift origin up to upper-left corner. */
+  g_gameLoop->UpdateWindowSize(w,h);
 }
 
 void
 display(void)
 {
-  glClear(GL_COLOR_BUFFER_BIT);
-  glBegin(GL_TRIANGLES);
-    glColor3f(0.0, 0.0, 1.0);  /* blue */
-    glVertex2i(0, 0);
-    glColor3f(0.0, 1.0, 0.0);  /* green */
-    glVertex2i(200, 200);
-    glColor3f(1.0, 0.0, 0.0);  /* red */
-    glVertex2i(20, 200);
-  glEnd();
+  g_gameLoop->Tick();
   glFlush();  /* Single buffered, so needs a flush. */
 }
 
 void StartLinuxApplication(int argc, char **argv)
 {
+  g_gameLoop = std::make_unique<GameLoop>();
+  auto config = IOCContainer::Instance().Resolve<Config>();
+  g_gameLoop->Initialize(config->FPS);
+  int width, height;
+  g_gameLoop->GetDefaultSize(width, height);
+  g_gameLoop->UpdateWindowSize(width, height);
+
   glutInit(&argc, argv);
-  glutCreateWindow("single triangle");
+  glutCreateWindow("a snake");
   glutDisplayFunc(display);
   glutReshapeFunc(reshape);
   glutMainLoop();
+  
+  g_gameLoop.reset();
 }
--- snake/source/linux/main.cpp
+++ snake/source/linux/main.cpp
@@ -1,9 +1,17 @@
+#include "IOC.hpp"
+#include "game/Game.h"
+#include "application/Config.h"
 
 void StartLinuxApplication(int argc, char** argv);
 
 int
 main(int argc, char **argv)
 {
+  auto config = std::make_shared<Engine::Config>();
+  config->FPS = 15;
+  Utilities::IOCContainer::Instance().Register<Engine::Config>(config);
+  Utilities::IOCContainer::Instance().Register<Engine::IGameLoopCallback>(std::make_shared<Game>());
+
   StartLinuxApplication(argc, argv);
   return 0;             /* ANSI C requires main to return int. */
 }
