684cd6615b75348f8e70b9e730f94df4c28b0d03
Johan Lindfors <Johan.Lindfors@truesec.se>
2023-01-01 10:50:33 +0100

    Still not working on osx

--- flappybird/resources/osx/shaders/fragment.glsl
+++ flappybird/resources/osx/shaders/fragment.glsl
@@ -1,9 +1,9 @@
 #version 330 core
 in vec2 TexCoords;
 out vec4 FragColor;
-uniform sampler2D texture1;
+uniform sampler2D tex;
 
 void main()
 {
-    FragColor = texture(texture1, TexCoords);
+    FragColor = texture(tex, TexCoords);
 }
