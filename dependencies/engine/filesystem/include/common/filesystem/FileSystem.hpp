d6ccdef26c99c5507e9d80345788182640db0266
Johan Lindfors <Johan.Lindfors@truesec.se>
2023-06-27 22:41:47 +0200

    Better getAssetsDirectory

--- dependencies/engine/filesystem/include/common/filesystem/FileSystem.hpp
+++ dependencies/engine/filesystem/include/common/filesystem/FileSystem.hpp
@@ -5,7 +5,11 @@ namespace Engine
 {
     class FileSystem final : public IFileSystem
     {
-        std::string getAssetsDirectory() override;
-        std::shared_ptr<File> loadFile(std::string filename, bool writeable) override;
+        public:
+            std::string getAssetsDirectory() override;
+            std::shared_ptr<File> loadFile(std::string filename, bool writeable) override;
+
+        private:
+            std::string mAssetsDirectory;
     };
 }
--- dependencies/engine/filesystem/source/common/FileSystem.cpp
+++ dependencies/engine/filesystem/source/common/FileSystem.cpp
@@ -13,10 +13,9 @@ using namespace Windows::ApplicationModel;
 #include <Windows.h>
 #include <vector>
 #include <filesystem>
-namespace fs = std::filesystem;
 #elif defined(PLATFORM_POSIX)
+#include <iostream>
 #include <filesystem>
-namespace fs = std::filesystem;
 #endif
 
 using namespace std;
@@ -24,29 +23,35 @@ using namespace Engine;
 
 string FileSystem::getAssetsDirectory()
 {
-	string path;
 	auto config = Utilities::IOCContainer::instance().resolve<Utilities::Config>();
+	if(mAssetsDirectory.size() == 0) {
+		cout << "Iterating over filesystem..." << endl;
 #ifdef UWP
-	const auto folder = Package::Current().InstalledLocation();
-	const auto folderPath = folder.Path();
-	path = std::string(folderPath + L"\\assets\\");    
+		const auto folder = Package::Current().InstalledLocation();
+		const auto folderPath = folder.Path();
+		mAssetsDirectory = string(folderPath + L"\\assets\\");    
 #elif WIN32
-	const unsigned int bufferSize = 512;
-	vector<char> buffer(bufferSize + 1);
-	::GetModuleFileName(nullptr, &buffer[0], bufferSize);
-	const string s = &buffer[0];
-	const fs::path p(s);
-	const auto executableDirectory = p.parent_path();
-	const auto folderPath = executableDirectory.generic_string();
-	path =  std::string(folderPath + "/assets/");
+		const unsigned int bufferSize = 512;
+		vector<char> buffer(bufferSize + 1);
+		::GetModuleFileName(nullptr, &buffer[0], bufferSize);
+		const string s = &buffer[0];
+		const filesystem::path p(s);
+		const auto executableDirectory = p.parent_path();
+		const auto folderPath = executableDirectory.generic_string();
+		mAssetsDirectory =  string(folderPath + "/assets/");
 #elif defined(PLATFORM_POSIX)
-	std::string current_working_dir(fs::current_path().generic_string());
-	path = string(current_working_dir + "/games/" + config->executable + "/assets/");
+		for (auto const& dir_entry : filesystem::recursive_directory_iterator(filesystem::current_path()))
+		{
+			if(dir_entry.is_directory() && dir_entry.path().filename() == "assets") {
+				mAssetsDirectory = string(dir_entry.path().generic_string() + "/");
+				break;
+			}
+		}
 #else
-	static_assert("Invalid platform configured");
+		static_assert("Invalid platform configured");
 #endif
-	printf("=== %s\n", path.c_str());
-    return path;
+	}
+	return mAssetsDirectory;
 }
 
 std::shared_ptr<File> FileSystem::loadFile(std::string filename, bool writeable)
