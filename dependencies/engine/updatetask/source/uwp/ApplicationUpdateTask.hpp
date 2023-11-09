30d334aeab43a2cb9706b2126d7576912c9886f9
Johan Lindfors <johan.lindfors@coderox.se>
2023-11-09 19:29:38 +0100

    Formatted the updatetask package

--- dependencies/engine/updatetask/source/uwp/ApplicationUpdateTask.hpp
+++ dependencies/engine/updatetask/source/uwp/ApplicationUpdateTask.hpp
@@ -2,10 +2,10 @@
 
 namespace updatetask
 {
-    [Windows::Foundation::Metadata::WebHostHidden]
-    public ref class ApplicationUpdateTask sealed : Windows::ApplicationModel::Background::IBackgroundTask {
+    [Windows::Foundation::Metadata::WebHostHidden] public ref class ApplicationUpdateTask sealed : Windows::ApplicationModel::Background::IBackgroundTask
+    {
     public:
-		ApplicationUpdateTask() { };
+        ApplicationUpdateTask(){};
 
         virtual void Run(Windows::ApplicationModel::Background::IBackgroundTaskInstance ^ taskInstance);
     };
