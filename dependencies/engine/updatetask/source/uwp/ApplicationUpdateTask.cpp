165e06a1f68b5dd3b63f0604f5c831e2c1fee607
Johan Lindfors <johan.lindfors@coderox.se>
2023-11-09 19:29:38 +0100

    Formatted the updatetask package

--- dependencies/engine/updatetask/source/uwp/ApplicationUpdateTask.cpp
+++ dependencies/engine/updatetask/source/uwp/ApplicationUpdateTask.cpp
@@ -11,7 +11,8 @@ using namespace Windows::System::UserProfile;
 using namespace Windows::Data::Xml::Dom;
 using namespace Windows::UI::Notifications;
 
-static Windows::Foundation::DateTime ToDateTime(long long unixTime) {
+static Windows::Foundation::DateTime ToDateTime(long long unixTime)
+{
 	FILETIME fileTime;
 	auto ll = (unixTime * 10000000) + 116444736000000000;
 
@@ -27,7 +28,8 @@ static Windows::Foundation::DateTime ToDateTime(long long unixTime) {
 	return dueTime;
 }
 
-static Windows::Foundation::DateTime GetDueDateTime(long long timeInSeconds) {
+static Windows::Foundation::DateTime GetDueDateTime(long long timeInSeconds)
+{
 	time_t t;
 	t = time(NULL);
 
@@ -35,32 +37,36 @@ static Windows::Foundation::DateTime GetDueDateTime(long long timeInSeconds) {
 
 	localtime_s(&tlm, &t);
 
-	if (timeInSeconds <= t) {
-		timeInSeconds = t + 5;// we need to make sure we don't try to send a notification to a time already passed, 5 seconds seems safe
+	if (timeInSeconds <= t)
+	{
+		timeInSeconds = t + 5; // we need to make sure we don't try to send a notification to a time already passed, 5 seconds seems safe
 	}
 
 	return ToDateTime(timeInSeconds);
 }
 
-String^ GetToastXml(String^ title, String^ message) {
+String ^ GetToastXml(String ^ title, String ^ message) {
 	auto toastXmlString = "<toast>" + "<visual version='2'>" + "<binding template='ToastText02'>" + "<text id='1'>" + title + "</text>" + "<text id='2'>" +
-		message + "</text>" + "</binding>" + "</visual>" + "</toast>";
+						  message + "</text>" + "</binding>" + "</visual>" + "</toast>";
 	return toastXmlString;
 }
 
-Windows::Foundation::IAsyncOperation<Windows::Data::Xml::Dom::XmlDocument^>^ LoadXmlAsync(Platform::String^ filename) {
-	return concurrency::create_async([filename]() {
+	Windows::Foundation::IAsyncOperation<Windows::Data::Xml::Dom::XmlDocument ^> ^
+	LoadXmlAsync(Platform::String ^ filename) {
+		return concurrency::create_async([filename]()
+										 {
 		auto folder = Windows::ApplicationModel::Package::Current->InstalledLocation;
 		return concurrency::create_task(folder->GetFileAsync(filename))
 			.then([](concurrency::task<Windows::Storage::StorageFile^> t) {
 			return Windows::Data::Xml::Dom::XmlDocument::LoadFromFileAsync(t.get());
-				});
-		});
-}
+				}); });
+	}
 
-void NotifyWithLocalXml(String^ filename, String^ id, DateTime dateTime) {
+	void NotifyWithLocalXml(String ^ filename, String ^ id, DateTime dateTime)
+{
 	create_task(LoadXmlAsync(filename))
-		.then([=](task<XmlDocument^> toastDomTask) {
+		.then([=](task<XmlDocument ^> toastDomTask)
+			  {
 		try {
 			auto toastDOM = toastDomTask.get();
 			auto toast = ref new ScheduledToastNotification(toastDOM, dateTime);
@@ -68,17 +74,21 @@ void NotifyWithLocalXml(String^ filename, String^ id, DateTime dateTime) {
 
 			ToastNotificationManager::CreateToastNotifier()->AddToSchedule(toast);
 		}
-		catch (...) { }
-	}).get();
+		catch (...) { } })
+		.get();
 }
 
-void ApplicationUpdateTask::Run(IBackgroundTaskInstance^ taskInstance) {
-	BackgroundTaskDeferral^ deferral = taskInstance->GetDeferral();
+void ApplicationUpdateTask::Run(IBackgroundTaskInstance ^ taskInstance)
+{
+	BackgroundTaskDeferral ^ deferral = taskInstance->GetDeferral();
 
-	try {
+	try
+	{
 		NotifyWithLocalXml("toast.xml", "GES", GetDueDateTime(5));
 	}
-	catch (...) { }
+	catch (...)
+	{
+	}
 
 	deferral->Complete();
 }
