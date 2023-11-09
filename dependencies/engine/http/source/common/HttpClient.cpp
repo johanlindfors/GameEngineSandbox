4f464bbba54e3fcd384e9195bb64af21ee726077
Johan Lindfors <johan.lindfors@coderox.se>
2023-11-09 19:20:50 +0100

    Formatted the http package

--- dependencies/engine/http/source/common/HttpClient.cpp
+++ dependencies/engine/http/source/common/HttpClient.cpp
@@ -6,41 +6,43 @@
 using namespace std;
 using namespace Engine;
 
-string CprHttpClient::get(string url) 
+string CprHttpClient::get(string url)
 {
     cpr::Response r = cpr::Get(cpr::Url{url});
     return r.text;
 }
 
-size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string* data) {
-    data->append((char*) ptr, size * nmemb);
+size_t writeFunction(void *ptr, size_t size, size_t nmemb, std::string *data)
+{
+    data->append((char *)ptr, size * nmemb);
     return size * nmemb;
 }
 
-string CurlHttpClient::get(string url) 
+string CurlHttpClient::get(string url)
 {
     std::string response_string;
     auto curl = curl_easy_init();
-    if (curl) {
+    if (curl)
+    {
         curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
         curl_easy_setopt(curl, CURLOPT_NOPROGRESS, 1L);
         // curl_easy_setopt(curl, CURLOPT_USERPWD, "user:pass");
         curl_easy_setopt(curl, CURLOPT_USERAGENT, "curl/7.42.0");
         curl_easy_setopt(curl, CURLOPT_MAXREDIRS, 50L);
         curl_easy_setopt(curl, CURLOPT_TCP_KEEPALIVE, 1L);
-        
+
         std::string header_string;
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writeFunction);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response_string);
         curl_easy_setopt(curl, CURLOPT_HEADERDATA, &header_string);
-        
-        char* effectiveUrl;
+
+        char *effectiveUrl;
         long response_code;
         double elapsed;
         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
         curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &elapsed);
         curl_easy_getinfo(curl, CURLINFO_EFFECTIVE_URL, &effectiveUrl);
-        
+
         curl_easy_perform(curl);
         curl_easy_cleanup(curl);
         curl = NULL;
